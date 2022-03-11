2dddf495d48c1077ee93ece874a35a8933696bbf
dd-hendon <d.hendon17@gmail.com>
2022-03-11 09:53:44 +0000

    Added error handling and displaying conditional message to the user

--- src/components/VoteBox.jsx
+++ src/components/VoteBox.jsx
@@ -5,36 +5,41 @@ import { patchVotes } from "../utils/api";
 
 export default function VoteBox({ votes, article_id }) {
   const [userVote, setUserVote] = useState(0);
+  const [err, setErr] = useState(null);
 
   const handleClick = (vote) => {
     setUserVote((currentVotes) => currentVotes + vote);
+    setErr(null);
     patchVotes(article_id, vote).catch((err) => {
-      console.log(err);
       setUserVote((currentVotes) => currentVotes - vote);
+      setErr("Something went wrong, please try again");
     });
   };
 
   return (
-    <section className="vote-box">
-      <button
-        className={`vote-button upvote `}
-        onClick={() => {
-          handleClick(1);
-        }}
-        disabled={userVote > 0}
-      >
-        <img src={upvote} alt="Upvote arrow using nc logo"></img>
-      </button>
-      <p className="vote-count">{votes + userVote}</p>
-      <button
-        className={`vote-button downvote `}
-        onClick={() => {
-          handleClick(-1);
-        }}
-        disabled={userVote < 0}
-      >
-        <img src={downvote} alt="Downvote arrow using nc logo"></img>
-      </button>
-    </section>
+    <>
+      <section className="vote-box">
+        <button
+          className={`vote-button upvote `}
+          onClick={() => {
+            handleClick(1);
+          }}
+          disabled={userVote > 0}
+        >
+          <img src={upvote} alt="Upvote arrow using nc logo"></img>
+        </button>
+        <p className="vote-count">{votes + userVote}</p>
+        <button
+          className={`vote-button downvote `}
+          onClick={() => {
+            handleClick(-1);
+          }}
+          disabled={userVote < 0}
+        >
+          <img src={downvote} alt="Downvote arrow using nc logo"></img>
+        </button>
+      </section>
+      <p className="vote-error-message">{err}</p>
+    </>
   );
 }
