e11a28da93ff812a86b788fc494455ac6d3b221b
dd-hendon <d.hendon17@gmail.com>
2022-03-11 11:36:10 +0000

    Create component

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,10 +1,12 @@
 import ArticleMain from "./ArticleMain";
 import CommentList from "./CommentList";
+import CommentPost from "./CommentPost";
 
 export default function Article({ article_id }) {
   return (
     <section id="article">
       <ArticleMain article_id={article_id} />
+      <CommentPost article_id={article_id} />
       <CommentList article_id={article_id} />
     </section>
   );
